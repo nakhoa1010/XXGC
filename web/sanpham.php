89ac916c63efd40a42d707deaa92c8c48fa5e0e7
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2022-12-22 19:38:55 +0700

    Update sanpham.php

--- web/sanpham.php
+++ web/sanpham.php
@@ -11,7 +11,7 @@ require_once ('../db/dbhelper.php');
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title> MOWO - MOTO WORLD </title>
+  <title> XSGC - Xe Xấu Giá Cao </title>
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Bootstrap core CSS -->
   <link href="../CSS/bootstrap.min.css" rel="stylesheet">
@@ -29,7 +29,7 @@ require_once ('../db/dbhelper.php');
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">  MOWO <i class="fa fa-motorcycle logo-icon"></i> </a>
+      <a class="navbar-brand" href="#"> XXGC <i class="fa fa-motorcycle logo-icon"></i> </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -139,7 +139,7 @@ require_once ('../db/dbhelper.php');
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; MOWO - MOTO WORLD 2020</p>
+      <p class="m-0 text-center text-white">Copyright &copy; XSGC - Xe Xấu Giá Cao</p>
     </div>
     <!-- /.container -->
   </footer>
