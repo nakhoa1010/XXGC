e8a21759a62f9780e25727bbbffb8983b06c0e42
nakhoa1010 <91071159+nakhoa1010@users.noreply.github.com>
2022-12-22 19:38:50 +0700

    Update dathang.php

--- web/dathang.php
+++ web/dathang.php
@@ -39,7 +39,7 @@ if(!empty($_POST)){
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title> MOWO - MOTO WORLD </title>
+        <title> XSGC - Xe Xấu Giá Cao </title>
         <link href="../CSS/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
@@ -129,7 +129,7 @@ if(!empty($_POST)){
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; MOWO - MOTO WORLD 2020</div>
+                            <div class="text-muted">Copyright &copy; XSGC - Xe Xấu Giá Cao</div>
                         </div>
                     </div>
                 </footer>
